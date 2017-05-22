535748bb6c81368f2a5c60c0debd1359cfbc5e65
d.thatmann@tu-berlin.de <d.thatmann@tu-berlin.de>
2017-05-22 19:10:48 +0200

    now loading path to config.py (containing passwdors) from local environment variables

--- src/at/__init__.py
+++ src/at/__init__.py
@@ -2,4 +2,6 @@ from flask import Blueprint
 
 at = Blueprint('at', __name__, static_folder='static', template_folder='templates', url_prefix='/admintool')
 
+
+
 import views
--- src/run.py
+++ src/run.py
@@ -1,2 +1,17 @@
 from bootstrap import app
+
+#yourapp/__init__.py
+
+#app = __name__, instance_relative_config=True
+
+# Load the default configuration
+#app.config.from_object('config.default')
+
+# Load the configuration from the instance folder
+#app.config.from_pyfile('config.py')
+
+# Load the file specified by the APP_CONFIG_FILE environment variable
+# Variables defined here will override those in the default configuration
+app.config.from_envvar('APP_CONFIG_FILE')
+
 app.run(host='0.0.0.0')
