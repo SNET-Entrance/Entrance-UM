dd36f32d8752238521d1b97e20d502e36320be08
d.thatmann@tu-berlin.de <d.thatmann@tu-berlin.de>
2017-05-23 10:41:15 +0200

    fixed IndentationError in app.py

--- src/run.py
+++ src/run.py
@@ -13,11 +13,10 @@ import logging
 # Load the file specified by the APP_CONFIG_FILE environment variable
 # Variables defined here will override those in the default configuration
 try:
-app.config.from_envvar('APP_CONFIG_FILE')
-app.config['HAS_CONFIG'] = True
+    app.config.from_envvar('APP_CONFIG_FILE')
+    app.config['HAS_CONFIG'] = True
 except: 
-logging.warning('Can\'t load conf file')
-app.config['HAS_CONFIG'] = False
+    logging.warning('Can\'t load conf file')
+    app.config['HAS_CONFIG'] = False
 finally:
-
-app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0')
