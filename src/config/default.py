f6022f8ef889d5ebdc034847b773082510acab16
Dirk <d.thatmann@tu-berlin.de>
2017-07-09 23:25:30 +0200

    d

--- src/config/default.py
+++ src/config/default.py
@@ -3,9 +3,6 @@ import os
 import socket
 import re
 
-def get_ip_address3():
-return [l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0]
-
 #enp0s31f6
 #ipv4 = re.search(re.compile(r'(?<=inet )(.*)(?=\/)', re.M), os.popen('ip addr show enp0s31f6').read()).groups()[0]
 
